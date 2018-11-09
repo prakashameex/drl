313c2fcfaa6a9dc51b6fcd8789bdd4549c1be711
kamal <kamal.prakash@ameexusa.com>
2018-11-09 21:05:15 +0530

    drl site setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { AllocationComponent } from './allocation/allocation.component';
+import {MatButtonModule, MatCheckboxModule,MatSelectModule,MatNativeDateModule,MatFormFieldModule,MatDatepickerModule, MatInputModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    AllocationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,MatButtonModule, MatCheckboxModule,MatDatepickerModule,MatSelectModule,MatNativeDateModule,MatFormFieldModule,MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
