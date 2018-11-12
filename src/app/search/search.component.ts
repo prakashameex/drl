c8f8437b721a31a79cc71f0ef3e5bd28d08c4753
kamal <kamal.prakash@ameexusa.com>
2018-11-12 21:24:26 +0530

    filter added

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,23 +2,25 @@ import { Component, OnInit,ViewChild } from '@angular/core';
 
 import {MatTableDataSource,MatSort} from '@angular/material';
 export interface PeriodicElement {
-  name: string;
+  description: string;
+  Assortment:string;
   position: number;
-  weight: number;
-  symbol: string;
+  Campaign: string;
+  begindate:string;  
+  enddate: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, description: 'Tacos',Assortment: 'sample',Campaign: 'test', begindate: 'Wed Oct 12 2014' ,enddate: 'Wed Oct 15 2014' },
+  {position: 2, description: 'Steak',Assortment: 'sample',Campaign: 'test', begindate: 'Wed Oct 13 2014' ,enddate: 'Wed Oct 16 2014' },
+  {position: 3, description: 'Pizza',Assortment: 'sample',Campaign: 'test', begindate: 'Wed Oct 14 2014' ,enddate: 'Wed Oct 17 2014' },
+  {position: 4, description: 'Pizza',Assortment: 'sample',Campaign: 'test', begindate: 'Wed Oct 15 2014' ,enddate: 'Wed Oct 18 2014' },
+  {position: 5, description: 'Pizza',Assortment: 'sample',Campaign: 'test', begindate: 'Wed Oct 16 2014' ,enddate: 'Wed Oct 19 2014' },
+  {position: 6, description: 'Carbon',Assortment: 'sample',Campaign: 'test', begindate: 'Wed Oct 17 2014' ,enddate: 'Wed Oct 20 2014' },
+  {position: 7, description: 'Nitrogen',Assortment: 'sample', Campaign: 'test', begindate: 'Wed Oct 13 2014' ,enddate: 'Wed Oct 21 2014' },
+  {position: 8, description: 'Pizza',Assortment: 'sample', Campaign: 'test', begindate: 'Wed Oct 18 2014' ,enddate: 'Wed Oct 22 2014' },
+  {position: 9, description: 'Fluorine',Assortment: 'sample', Campaign: 'test', begindate: 'Wed Oct 13 2014' ,enddate: 'Wed Oct 23 2014' },
+  {position: 10, description: 'Pizza',Assortment: 'sample', Campaign: 'test', begindate: 'Wed Oct 19 2014' ,enddate: 'Wed Oct 5 2014' },
 ];
 
 /*** @title Table with filtering
@@ -35,16 +37,19 @@ export class SearchComponent implements OnInit {
     {value: 'pizza', viewValue: 'Pizza'},
     {value: 'tacos', viewValue: 'Tacos'}
   ];
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns = ['position', 'description','Assortment','Campaign', 'begindate','enddate'];
     dataSource = new MatTableDataSource(ELEMENT_DATA);
     @ViewChild(MatSort) sort: MatSort;
-    applyFilter(filterValue) {   
-      console.log('filer obj',filterValue)   
-      for(var key in filterValue){
-        console.log('key values',filterValue[key]);
-      this.dataSource.filter = filterValue[key].trim().toLowerCase();
-      }
+    applyFilter(filterValue:string) {   
+      console.log('filer obj',filterValue)         
+    this.dataSource.filter = filterValue.trim().toLowerCase();
     }
+    datefilter(filterValue) {   
+      console.log('date filer obj',filterValue.toDateString())      
+     
+    this.dataSource.filter = filterValue.toDateString().trim();
+    console.log(this.dataSource.filteredData[0]) 
+  }
   constructor() {
     
    }
